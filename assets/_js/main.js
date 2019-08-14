2d804bd3996bef6c88c1730cec0f3b90e0b051a2
Jovan <jovan.saviic@gmail.com>
2019-08-15 00:53:44 +0200

    full refactor

--- assets/_js/main.js
+++ assets/_js/main.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import 'jquery-touchswipe';
 
 $(window).on('load', function() {
     Common.init();
--- frasco.config.js
+++ frasco.config.js
@@ -55,6 +55,7 @@ module.exports = {
     dest:  'js',
     entry: [
       'bundle.js',
+      // 'swipe.js',
       'main.js',
     ],
   },
